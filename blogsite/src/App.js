46a148871ff80d1960be833edd680e3a98326d54
Divya <karkidivya5@gmail.com>
2024-04-30 14:01:44 +0545

    material ui added

--- blogsite/src/App.js
+++ blogsite/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <Signup /> },
+      
+    ]
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
